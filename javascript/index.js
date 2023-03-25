3b6bb8eab4b4681799ee9b2fe15dae5ca178e312
Ashley Ferris <ashleyferris6@gmail.com>
2023-03-25 12:19:26 -0700

    Added the current location
    - Added "My current location" as a select option
    - Updated the function updateCity so that it can guess the user's current timezone location

--- javascript/index.js
+++ javascript/index.js
@@ -41,6 +41,9 @@ setInterval(updateTime, 1);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
